c6e9eef52413b0980bf9bf5cf364985dbdc9b16b
Adrian Federico Toledo Campoverde <pq17tmn@gmail.com>
2024-02-27 16:27:15 +0100

    Adrian Dossier with authentificaction, users and books put in
    Authentification with Jetons  for Users and passwords
    DB from books and users with mock works well

---
+++ Adrian/LovBooks/src/model/t_book.mjs
@@ -0,0 +1,73 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db/sequelize.mjs";
+
+//Model de table de "book"
+const BookModelTable = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "BOOK",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      number_of_pages: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        //Validation requisent
+        validate: {
+          isInt: {
+            //Message d'erreur
+            msg: "Le nombre de pages doit être un entier.",
+          },
+          min: {
+            args: [1],
+            //Message d'erreur
+            msg: "Le livre doit avoir au moins une page.",
+          },
+        },
+      },
+      year_of_publication: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        //Validation requisent
+        validate: {
+          isInt: {
+            //Message d'erreur
+            msg: "L'année de publication doit être un entier.",
+          },
+        },
+      },
+      average_ratings: {
+        type: DataTypes.FLOAT,
+      },
+      cover_image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      extract_pdf: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      summary: {
+        type: DataTypes.TEXT,
+      },
+    },
+    //Colonne créer automatiquement et ajouté à la table
+    {
+      timestamps: true,
+      createdAt: "created",
+      updatedAt: false,
+    }
+  );
+};
+
+export { BookModelTable };
