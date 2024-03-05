a6af072c68d27bf8ec2d5a76714e33283e3b3d7d
Mathis Botteau <123393731+Froglaprod@users.noreply.github.com>
2024-03-05 10:28:24 +0100

    Liaisons livres clef étrangère
    Liaisons de la t_book avec tout les tables / création routes category

--- Mathis/LovBooks/src/app.mjs
+++ Mathis/LovBooks/src/app.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import { sequelize, initDB } from "./db/sequelize.mjs";
 import { booksRouter } from "./routes/bookRoutes.mjs";
 import { customersRouter } from "./routes/customerRoutes.mjs";
+import { categorysRouter } from "./routes/categoryRoutes.mjs";
 
 const app = express();
 const port = 3420;
@@ -10,9 +11,9 @@ app.use(express.json());
 
 //Authentification et synchronisation de la db
 sequelize
- .authenticate()
- .then((_) => console.log("La connexion à la base de données a bien été établie"))
- .catch((error)=>console.error("Impossible de se connecter à la DB"))
+    .authenticate()
+    .then((_) => console.log("La connexion à la base de données a bien été établie"))
+    .catch((error) => console.error("Impossible de se connecter à la DB"))
 initDB();
 
 app.get("/", (req, res) => {
@@ -29,9 +30,12 @@ app.use("/api/books", booksRouter);
 //Utilise la route pour les users
 app.use("/api/users", customersRouter);
 
+//Utilise la route pour les categories
+app.use("/api/categorys", categorysRouter);
+
 //Erreur 404
-app.use(({res})=>{
-    const message= "Impossible de trouver la ressource demandée! Vous pouvez essayer une autre URL."
+app.use(({ res }) => {
+    const message = "Impossible de trouver la ressource demandée! Vous pouvez essayer une autre URL."
     res.status(404).json(message)
 })
 
